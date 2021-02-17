f4348c2aa00dcabd897436dd4b53e9d7baf0d486
Thibaut COUTURIER <couturier@newcefe.newage.fr>
2021-02-17 16:39:20 +0100

    boroullon suite

--- scripts/brouillon.R
+++ scripts/brouillon.R
@@ -1,2 +1,6 @@
 # fonction corrplot
 corrplot::corrplot(CorTable, type="upper", order="hclust", tl.col="black", tl.srt=45)
+
+# création masques
+maskzone<-rasterize(pnm, zone, background=NA)
+alt_PNM <- mask(alt_PNM, maskzone)
