8e2aa942525510d02d48b1490b748beb125f0c97
Thibaut COUTURIER <couturier@newcefe.newage.fr>
2021-02-18 19:15:04 +0100

    qqes broutilles sur le script3. Création du readme

--- README.md
+++
@@ -1,1 +0,0 @@
-# LEPUS_SDM
--- scripts/3 - PREPARATION-DONNEES-PRESENCE-ABSENCE.R
+++ scripts/3 - PREPARATION-DONNEES-PRESENCE-ABSENCE.R
@@ -23,8 +23,8 @@ resultats_bruts_lots_plusieurs_crottes<-read_csv2("./data/crotte/resultats_bruts
 resultats_bruts_crottes_seules<-read_csv2("./data/crotte/resultats_bruts_crottes_seules.csv")
 
 resultats_bruts_lots_plusieurs_crottes %>%
-  select(id_lot, Conclusion) %>%
-  rename(espece = Conclusion) -> plusieurs_crottes
+  dplyr::select(id_lot, Conclusion) %>%
+  dplyr::rename(espece = Conclusion) -> plusieurs_crottes
 
 resultats_bruts_crottes_seules %>%
   dplyr::rename(espece = `Assignation espèce`) %>%
@@ -40,34 +40,34 @@ resultats_bruts_crottes_seules %>%
   dplyr::select(-espece) -> crottes_assemblees
 
 
-# importation des points de collecte 
+# importation des shapes des points de collecte 
 points_collecte_lots_2020<-sf::st_read(dsn = "data/crotte", layer = "points_collecte_lots_2020")
 points_collecte_lots_2020 %>% 
-  st_cast("POINT") -> points_collecte_lots_2020 # ici cette fonction car la géométrie "multipoint" n'est pas acceptée lors de la rasterisation
+  sf::st_cast("POINT") -> points_collecte_lots_2020 # ici cette fonction car la géométrie "multipoint" n'est pas acceptée lors de la rasterisation
 
 
 # jointure : attention, 28 données avec timidus et europaeus en NA (correspondent certainement à celles filtrées au-dessus, mais j'ai pas vérifié) -> supprimés
 points_collecte_lots_2020 %>%
-  left_join(crottes_assemblees, by = c("id" = "id_lot")) %>%
-  filter(!is.na(timidus)) -> points_collecte_espece 
+  dplyr::left_join(crottes_assemblees, by = c("id" = "id_lot")) %>%
+  dplyr::filter(!is.na(timidus)) -> points_collecte_espece 
 
 
-# on rasterise les points timidus et europaeus
+# rasterisation des points timidus et europaeus
 raster<-raster::raster("./data/rasters/alt_PNM_focal400.asc")
 projection(raster)<-CRS("+init=EPSG:2154")
 raster[]<-0
-raster_timidus<-rasterize(points_collecte_espece, field="timidus", raster,  background=NA)
-raster_europaeus<-rasterize(points_collecte_espece, field="europaeus", raster,  background=NA)
+raster_timidus<-raster::rasterize(points_collecte_espece, field="timidus", raster,  background=NA)
+raster_europaeus<-raster::rasterize(points_collecte_espece, field="europaeus", raster,  background=NA)
 
 
 # on convertit le raster en polygones pour recuperer les centroides des mailles avec présence de chaque espece 
 raster_timidus[raster_timidus==0]<-NA
-polygon_timidus<-rasterToPolygons(raster_timidus)
+polygon_timidus<-raster::rasterToPolygons(raster_timidus)
 timidus_hiver20<-as.data.frame(coordinates(polygon_timidus))
 colnames(timidus_hiver20)<-c("x","y")
 
 raster_europaeus[raster_europaeus==0]<-NA
-polygon_europaeus<-rasterToPolygons(raster_europaeus)
+polygon_europaeus<-raster::rasterToPolygons(raster_europaeus)
 europaeus_hiver20<-as.data.frame(coordinates(polygon_europaeus))
 colnames(europaeus_hiver20)<-c("x","y")
 
@@ -84,26 +84,25 @@ colnames(europaeus_hiver20)<-c("x","y")
 traces <- sf::st_read(dsn = "data/trace", layer = "traces_prospection_2020")
 # long_traces<-rasterize(traces, raster, field='id_serial', fun='length') # calcul de la longeur de transect par sous-maille très long : réalisé par le cluster et intégré dans output
 long_traces<-raster::raster("./outputs/long_traces.asc")
-projection(long_traces)<-CRS("+init=EPSG:2154")
+raster::projection(long_traces)<-CRS("+init=EPSG:2154")
 traces <- long_traces
 traces[traces == 0] <- NA
 traces[traces > 0] <- 1 # mailles avec 1 si parcouru par l'observateur (même sur qqes mètres)
-
 
 
 #######################
 # préparation des données en présence-absence 
 #######################
 
-# on crée un dataframe avec les coordonnées des mailles 100m prospectées (centroïdes) et une colonne "pres" avec 0.
-polygon_traces<-rasterToPolygons(traces)
-coord_traces<-as.data.frame(coordinates(polygon_traces))
+# création dataframe avec les coordonnées (centroïdes) des mailles 100m prospectées et une colonne "pres" avec 0.
+polygon_traces<-raster::rasterToPolygons(traces)
+coord_traces<-as.data.frame(raster::coordinates(polygon_traces))
 pres <- vector(length = dim(coord_traces)[1])
 pres[]<-0
 coord_traces <- cbind(coord_traces, pres)
 colnames(coord_traces)<-c("x","y","pres")
 
-# le tableau de coordonnées avec présences
+# le tableau de coordonnées avec présences (voir au-dessus)
 pres_timidus <- vector(length = dim(timidus_hiver20)[1])
 pres_timidus[]<-1
 coord_timidus <- cbind(timidus_hiver20, pres_timidus)
@@ -114,14 +113,14 @@ pres_europaeus[]<-1
 coord_europaeus <- cbind(europaeus_hiver20, pres_europaeus)
 colnames(coord_europaeus)<-c("x","y","pres")
 
-# on assemble les deux tableaux et on supprime les doublons (centroïdes avec lièvre et traces) en ne conservant  que les 1 
+# assemblage des deux tableaux et suppression des doublons (centroïdes avec lièvre et traces) en ne conservant  que les 1 
 assemblage_timidus <- rbind(coord_timidus, coord_traces)
 coord_timidus_presabs20<-assemblage_timidus[!duplicated(assemblage_timidus[,1:2]),]
 
 assemblage_europaeus <- rbind(coord_europaeus, coord_traces)
 coord_europaeus_presabs20<-assemblage_europaeus[!duplicated(assemblage_europaeus[,1:2]),]
 
-
+# suite, auformat biomod... (facultatif)
 myRespCoordp<-coord_europaeus_presabs20
 myRespCoord<-myRespCoordp[,1:2]
 myResp <- as.numeric(myRespCoordp[,3])
